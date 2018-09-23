7f10e1c327a48c9b908ff3b2d53474a8760f718b
Scott <scot@udel.edu>
2018-09-23 14:36:24 -0400

    pulling the code

--- script/pizzaUI.js
+++ script/pizzaUI.js
@@ -1,6 +1,7 @@
 var pizzaUI = function(){
     var self = this;
     this.game = undefined;
+    window.flag = 0; 
     this.initialize=function(){
         game = new pizzaGame();
         console.log(game);
@@ -8,6 +9,7 @@ var pizzaUI = function(){
     this.refreshView=function(){
         $('#pizza')
     }
+    
     this.update = function(){
         if ($(window).width() + ($(window).width() * .1) < game.options.currentPizzaPosition) {
             game.options.currentPizzaPosition=-($(window).width() * .1);
@@ -15,8 +17,20 @@ var pizzaUI = function(){
         else {
             game.options.currentPizzaPosition+=1;
         }
+        var x = $('#topping').position();
+        //console.log(x, game.options.currentPizzaPosition);
+        var right = game.options.currentPizzaPosition+70;
         $('#pizza').css("left",game.options.currentPizzaPosition+'px');
+        if(window.flag == 1) $('#topping').css("left",game.options.currentPizzaPosition+'px');
+
+        
+        if(x.left > game.options.currentPizzaPosition && x.left<right && (x.top > $('#pizza').position().top)){
+            console.log("kjf");
+            window.flag = 1;
+           
+        }
     }
     setInterval(update, 10);
     this.initialize();
+    
 }
